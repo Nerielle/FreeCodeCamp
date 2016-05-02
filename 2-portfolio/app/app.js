d3bd17134f96a7b46862407343c299780b313045
Nerielle <nelya.iva@gmail.com>
2016-05-02 16:17:59 +0300

    +carosel from bootstrap

--- 2-portfolio/app/app.js
+++ 2-portfolio/app/app.js
@@ -2,11 +2,32 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ngRoute'
+    // 'myApp.view1',
+    // 'myApp.view2',
+]);
+// ]);.config(['$routeProvider', function ($routeProvider) {
+//     $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
+
+angular.module('myApp')
+    .controller('appController', function ($scope) {
+        $scope.projects = [
+            {
+                name: "Some nice project",
+                description: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+                imgSrc: "http://www.freeimageslive.com/galleries/backdrops/fractal/pics/fractal_overlap_curves.jpg"
+            },
+            {
+                name: "Project 2",
+                description: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+                imgSrc: "http://www.freeimageslive.com/galleries/backdrops/fractal/pics/fractal_highlights_blue.jpg"
+            },
+            {
+                name: "Project 3",
+                description: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+                imgSrc: "http://www.freeimageslive.com/galleries/backdrops/fractal/pics/fractal_cubes_cyan.jpg"
+            }
+        ];
+    });
+
