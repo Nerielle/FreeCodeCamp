8166f56e04092cd1df7578bb774fe84dcc59b479
Nerielle <nelya.iva@gmail.com>
2016-05-09 22:31:53 +0300

    +tweet button implementation. *styles

--- 3 - get a quote/js/index.js
+++ 3 - get a quote/js/index.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
     $.ajaxSetup({cache: false});
+    getNewQuote();
+});
+$('.twitter-share-button').click(function () {
+    window.open('https://twitter.com/intent/tweet?text="' +
+        $('#content').text() + '" ' +
+        $('#author').text());
 });
 $('button').click(function () {
+    getNewQuote();
+});
+function changeText(response) {
+    var content = $(response.content).text();
+    $("#content").text(content);
+    $("#author").text(response.title);
+}
 
+function getNewQuote() {
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (a) {
         var quote = $('.quote');
-        quote.addClass('animated hinge');
-        quote.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-            var content = $(a[0].content).text();
-            $(".quote .content").text(content);
-            $(".quote .author").text(a[0].title);
-            quote.removeClass('animated hinge');
-        });
+        if ($("#content").text() == "") {
+            changeText(a[0])
+        } else {
+            quote.addClass('animated hinge');
+            quote.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                changeText(a[0]);
+                quote.removeClass('animated hinge');
+            });
+        }
 
     });
-});
+}
+
