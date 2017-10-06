1de0d078ad69ebcc7939f2a5c52bf0246ca20072
Nerielle <nelya.iva@gmail.com>
2017-10-06 19:58:51 +0300

    sign selection added and game restart

--- 8 - Tic-tac-toe/js/index.js
+++ 8 - Tic-tac-toe/js/index.js
@@ -5,54 +5,65 @@ function appController() {
     var controller = this;
     var player1Sign = 'X';
     var player2Sign = 'O';
-
     var winRows = [
-            ["1", "2", "3"],
-            ["4", "5", "6"],
-            ["7", "8", "9"],
-            ["1", "4", "7"],
-            ["2", "5", "8"],
-            ["3", "6", "9"],
-            ["1", "5", "9"],
-            ["3", "5", "7"]
+            ["1", "2", "3"]
+            , ["4", "5", "6"]
+            , ["7", "8", "9"]
+            , ["1", "4", "7"]
+            , ["2", "5", "8"]
+            , ["3", "6", "9"]
+            , ["1", "5", "9"]
+            , ["3", "5", "7"]
         ];
-
     activate();
 
     function activate() {
+        controller.play = play;
+        controller.start = start;
+        controller.setPlayerSign = setPlayerSign;
+        start();
+    }
+
+    function setPlayerSign(sign) {
+        if (player1Sign !== sign) {
+            var secondSign = player1Sign;
+            player1Sign = sign;
+            player2Sign = secondSign;
+        }
+        controller.showSignSelection = false;
+    }
+
+    function start() {
         /*
             1 | 2 | 3
             4 | 5 | 6
             7 | 8 | 9
         */
         controller.currentPosition = {
-            1: '',
-            2: '',
-            3: '',
-            4: '',
-            5: '',
-            6: '',
-            7: '',
-            8: '',
-            9: ''
+            1: ''
+            , 2: ''
+            , 3: ''
+            , 4: ''
+            , 5: ''
+            , 6: ''
+            , 7: ''
+            , 8: ''
+            , 9: ''
         };
-
         controller.isPlayer1Turn = true;
         controller.onePlayerGame = true;
         controller.gameOver = '';
-        controller.play = play;
-
+        controller.showSignSelection = true;
     }
 
     function play(pos) {
         if (controller.gameOver !== '') {
-            return
+            return;
         }
         if (controller.currentPosition[pos] !== '') {
             return;
         }
         controller.currentPosition[pos] = controller.isPlayer1Turn ? player1Sign : player2Sign;
-
         if (isGameOver()) {
             return;
         }
@@ -66,23 +77,19 @@ function appController() {
     }
 
     function isGameOver() {
-
         var crosses = [];
         var noughts = [];
         _.forOwn(controller.currentPosition, (value, key) => {
             if (value === player1Sign) {
                 crosses.push(key);
-            } else if (value === player2Sign) {
+            }
+            else if (value === player2Sign) {
                 noughts.push(key);
             }
-
         });
-
-
         _.forEach(winRows, winRow => {
             var intersectWithCrosses = _.intersection(winRow, crosses);
             var intersectWithNoughts = _.intersection(winRow, noughts);
-
             if (intersectWithCrosses.length === 3) {
                 controller.gameOver = 'Crosses won!'
                 return;
@@ -96,8 +103,6 @@ function appController() {
                 return;
             }
         });
-
-
     }
 
     function selectNextCell() {
@@ -106,18 +111,15 @@ function appController() {
         _.forOwn(controller.currentPosition, (value, key) => {
             if (value === player1Sign) {
                 crosses.push(key);
-            } else if (value === player2Sign) {
+            }
+            else if (value === player2Sign) {
                 noughts.push(key);
             }
-
         });
-
         _.forEach(winRows, winRow => {
             var intersectWithCrosses = _.intersection(winRow, crosses);
             var intersectWithNoughts = _.intersection(winRow, noughts);
-
             if (intersectWithCrosses.length === 2) {
-
                 var third = _.difference(winRow, intersectWithCrosses);
                 controller.currentPosition[third] = player2Sign;
             }
