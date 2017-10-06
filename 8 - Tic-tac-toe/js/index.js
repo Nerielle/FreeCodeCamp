57b0654ad3a5368ec2c9e3cbbb5bfe6cdc802e40
Nerielle <nelya.iva@gmail.com>
2017-10-06 22:00:10 +0300

    added timeout for computer turn; updated logic for computer ( it won't find the best cell to win)

--- 8 - Tic-tac-toe/js/index.js
+++ 8 - Tic-tac-toe/js/index.js
@@ -1,10 +1,12 @@
 var app = angular.module('tictactoe', []);
 app.controller('appController', appController);
 
-function appController() {
+function appController($timeout) {
     var controller = this;
-    var player1Sign = 'X';
-    var player2Sign = 'O';
+    var player1Sign = '';
+    var player2Sign = '';
+    var xSign = 'X';
+    var oSign = 'O';
     var winRows = [
             ["1", "2", "3"]
             , ["4", "5", "6"]
@@ -25,11 +27,8 @@ function appController() {
     }
 
     function setPlayerSign(sign) {
-        if (player1Sign !== sign) {
-            var secondSign = player1Sign;
-            player1Sign = sign;
-            player2Sign = secondSign;
-        }
+        player1Sign = sign;
+        player2Sign = sign === xSign ? oSign : xSign;
         controller.showSignSelection = false;
     }
 
@@ -57,78 +56,115 @@ function appController() {
     }
 
     function play(pos) {
+        if (player1Sign === '') {
+            return;
+        }
         if (controller.gameOver !== '') {
             return;
         }
         if (controller.currentPosition[pos] !== '') {
             return;
         }
+        if (controller.isPlayer1Turn === false) {
+            //wait for the computer
+            return;
+        }
         controller.currentPosition[pos] = controller.isPlayer1Turn ? player1Sign : player2Sign;
-        if (isGameOver()) {
+        console.log('gamer')
+        checkGameOver();
+        if (controller.gameOver !== '') {
             return;
         }
         controller.isPlayer1Turn = !controller.isPlayer1Turn;
-        if (!controller.isPlayer1Turn && controller.onePlayerGame) {
-            //delay
-            selectNextCell();
-            isGameOver();
-            controller.isPlayer1Turn = !controller.isPlayer1Turn;
+        if (controller.isPlayer1Turn === false && controller.onePlayerGame) {
+            $timeout(function () {
+                selectNextCell();
+                console.log('end', controller.currentPosition);
+                checkGameOver();
+                controller.isPlayer1Turn = !controller.isPlayer1Turn;
+            }, 1000);
         }
     }
 
-    function isGameOver() {
-        var crosses = [];
-        var noughts = [];
+    function checkGameOver() {
+        var player1cells = [];
+        var player2cells = [];
         _.forOwn(controller.currentPosition, (value, key) => {
             if (value === player1Sign) {
-                crosses.push(key);
+                player1cells.push(key);
             }
             else if (value === player2Sign) {
-                noughts.push(key);
+                player2cells.push(key);
             }
         });
-        _.forEach(winRows, winRow => {
-            var intersectWithCrosses = _.intersection(winRow, crosses);
-            var intersectWithNoughts = _.intersection(winRow, noughts);
-            if (intersectWithCrosses.length === 3) {
-                controller.gameOver = 'Crosses won!'
-                return;
+        _.each(winRows, winRow => {
+            var intersectWithPlayer1Cells = _.intersection(winRow, player1cells);
+            var intersectWithPlayer2Cells = _.intersection(winRow, player2cells);
+            if (intersectWithPlayer1Cells.length === 3) {
+                controller.gameOver = 'You won!'
+                return false;
             }
-            if (intersectWithNoughts === 3) {
-                controller.gameOver = "Noughts won!";
-                return;
+            if (intersectWithPlayer2Cells.length === 3) {
+                controller.gameOver = "Computer won!";
+                return false;
             }
-            if (crosses.length + noughts.length === 9) {
+            if (player1cells.length + player2cells.length === 9) {
                 controller.gameOver = "Draw!";
-                return;
+                return false;
             }
         });
     }
 
     function selectNextCell() {
-        var crosses = [];
-        var noughts = [];
+        console.log('comp')
+        var player1cells = [];
+        var player2cells = [];
+        var emptyCells = [];
+        var selected = false;
         _.forOwn(controller.currentPosition, (value, key) => {
             if (value === player1Sign) {
-                crosses.push(key);
+                player1cells.push(key);
             }
             else if (value === player2Sign) {
-                noughts.push(key);
+                player2cells.push(key);
+            }
+            else {
+                emptyCells.push(key);
             }
         });
+        console.log(player1cells, player2cells, emptyCells);
+        //find win row or don't give up ( computer is not 100% smart)
         _.forEach(winRows, winRow => {
-            var intersectWithCrosses = _.intersection(winRow, crosses);
-            var intersectWithNoughts = _.intersection(winRow, noughts);
-            if (intersectWithCrosses.length === 2) {
-                var third = _.difference(winRow, intersectWithCrosses);
-                controller.currentPosition[third] = player2Sign;
+            var intersectWithPlayer2Cells = _.intersection(winRow, player2cells);
+            if (intersectWithPlayer2Cells.length === 2) {
+                var third = _.difference(winRow, intersectWithPlayer2Cells);
+                if (controller.currentPosition[third] === '') {
+                    controller.currentPosition[third] = player2Sign;
+                    selected = true;
+                    console.log('w', third)
+                }
             }
-            if (intersectWithNoughts === 2) {
-                var third = _.difference(winRow, intersectWithNoughts);
-                controller.currentPosition[third] = player2Sign;
+            if (selected) {
+                return false;
             }
-            //or any empty cell in a row
-            console.log(controller.currentPosition);
+            var intersectWithPlayer1Cells = _.intersection(winRow, player1cells);
+            if (intersectWithPlayer1Cells.length === 2) {
+                var third = _.difference(winRow, intersectWithPlayer1Cells);
+                if (controller.currentPosition[third] === '') {
+                    controller.currentPosition[third] = player2Sign;
+                    selected = true;
+                    console.log('g', third)
+                }
+            }
         });
+        if (selected) {
+            return;
+        }
+        //mark any empty cell
+        if (selected === false) {
+            var randomEmptyCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+            console.log(randomEmptyCell);
+            controller.currentPosition[randomEmptyCell] = player2Sign;
+        }
     }
 }
