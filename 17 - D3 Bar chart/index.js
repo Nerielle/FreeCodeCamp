c821acef1d9c3c7d166a1b31aa61dfa18fcdbdbd
Nerielle <nelya.iva@gmail.com>
2020-04-30 21:12:06 +0300

    right heights

--- 17 - D3 Bar chart/index.js
+++ 17 - D3 Bar chart/index.js
@@ -1,66 +1,71 @@
 
-fetch('https://api.covid19api.com/summary')
+/*fetch('https://api.covid19api.com/summary')
   .then(response => {
     return response.json();
   })
   .then((data) => {
-    console.log(data);
-   // let c = document.getElementById('c');
+    console.log(data);*/
+    let c = document.getElementById('c');
   //c.textContent = JSON.stringify(data);
-  let dataset = data.Countries.map(c => [c.Country, c.TotalConfirmed]);
+  let dataset = [['UK',600],['R', 100],['Jggfh',200]];//data.Countries.map(c => [c.Country, c.TotalConfirmed]);
   //c.textContent = JSON.stringify(dataset);
-    //[['UK',390],['R', 303],['Jggfh',204]];
   let padding = 30;
-  let height = 600;
-  let width=680;
+  let height = 300;
+  let width=300;
   var maxDataValue = d3.max(dataset, d=> d[1]);
 
-
     const xScale = d3.scaleLinear()
-                     .domain([0, maxDataValue])
-                     .range([padding, width - padding]);
+                     .domain([0, dataset.length*30])
+                     .range([0, width - padding]);
+
+    const linearScale = d3.scaleLinear()
+     .domain([0,d3.max(dataset, (d) => d[1])])
+                     .range([0 , height - padding]);
 
     const yScale = d3.scaleLinear()
-                      .domain([0, maxDataValue])
-                     .range([height - padding, padding]);
-let svg =  d3.select('section')
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
+                     .domain([0, d3.max(dataset, (d) => d[1])])
+                     .range([height - padding , 0]);
 
-const xAxis = d3.axisBottom(xScale);
-    svg.append("g")
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .call(xAxis);
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", width)
+                  .attr("height", height);
 
-const yAxis = d3.axisLeft(yScale);
-    svg.append("g")
-    .attr("transform", "translate(" + padding + ", 0)")
-    .call(yAxis);
-      
-function getXAxesShiftForBar(i){
-    return  xScale(i ) * padding;
-}
-svg.selectAll('rect')
-  .data(dataset)
-  .enter()
-  .append('rect')
-       .attr("x", (d, i) => getXAxesShiftForBar(i))
-       .attr("y", (d, i) =>height -  yScale(d[1]) - padding)
-       .attr("width", 25)
-       .attr("height", (d, i) => yScale(d[1]))
-       .attr("fill", "navy");
+    svg.selectAll("rect")
+       .data(dataset)
+       .enter()
+       .append("rect")
+       .attr("x", (d,i) =>  i*30 + padding )
+       .attr("y",(d) => height - linearScale(d[1]) - padding)
+       .attr('width', '25')
+       .attr('height', (d,i)=> linearScale(d[1]));
+       //.attr("rx",'5');
 
-   svg.selectAll("text")
+    svg.selectAll("text")
        .data(dataset)
        .enter()
        .append("text")
-       .text((d) => d[0])
-       .attr("x", (d, i) => getXAxesShiftForBar(i))
-       .attr("y", (d, i) => height -  yScale(d[1]) );
-  
-       
-    
+       .text((d) =>  (d[0] + "," + linearScale(d[1])))
+       .attr("x", (d,i) => i * 30 + padding)
+       .attr("y", (d) => linearScale(d[1]) - 5)
 
-  });
+    const xAxis = d3.axisBottom(xScale);
+    // Add your code below this line
+    const yAxis = d3.axisLeft(yScale);
+    // Add your code above this line
+
+    svg.append("g")
+       .attr("transform",'translate('+padding+','+ (height - padding)  +")")
+.style('fill','red')
+       .call(xAxis);
+
+    // Add your code below this line
+    svg.append('g')
+    .attr('transform', 'translate('+padding +' , 0)')
+    .style('fill','red')
+    .call(yAxis);
+   
+
+
+ // });
  
