9e806f51f6d6da8dc76aceacb21c923092e29b54
Nerielle <nelya.iva@gmail.com>
2020-05-02 17:02:47 +0300

    RUSSIA covid cases

--- 17 - D3 Bar chart/index.js
+++ 17 - D3 Bar chart/index.js
@@ -1,34 +1,32 @@
-
-fetch('https://api.covid19api.com/summary')
+//  https://api.covid19api.com/live/country/russian-federation
+//https://api.covid19api.com/summary 
+fetch('https://api.covid19api.com/live/country/russian-federation')
   .then(response => {
     return response.json();
   })
   .then((data) => {
-  
-    //let c = document.getElementById('c');
+    console.log(data);
+  var ll = ['Confirmed', 'Deaths', 'Recovered', 'Active'];
+  var res = ['Korea (South)', 'Belarus', 'Australia', 'China', 'Canada', 'France', 'Denmark', 'New Zealand', 'Russian Federation', 'United States of America', 'United Kingdom', 'Kazakhstan', 'India', 'Germany'];
+  //let c = document.getElementById('c');
   //c.textContent = JSON.stringify(data);
-  let dataset =data.Countries.map(c => [c.Country, c.TotalConfirmed])
-  .filter(c =>{
-      console.log(c[0]);
-     var res= ['Korea (South)','Belarus','Australia','China','Canada','France','Denmark','New Zealand','Russian Federation','United States of America','United Kingdom','Kazakhstan','India','Germany'].includes(c[0]);
-    return res;
-  });
+  let dataset = data.map(c => c.Active);
   console.log(dataset);
   let padding = 30;
   let height =600;
   let width=800;
-  var maxDataValue = d3.max(dataset, d=> d[1]);
+  var maxConfirmedValue = d3.max(dataset, d=> d);
 
     const xScale = d3.scaleLinear()
                      .domain([0, dataset.length*10])
                      .range([0, width - padding*2]);
 
     const linearScale = d3.scaleLinear()
-     .domain([0,d3.max(dataset, (d) => d[1])])
+     .domain([0,d3.max(dataset, (d) => d)])
                      .range([0 , height - padding*2]);
 
     const yScale = d3.scaleLinear()
-                     .domain([0, d3.max(dataset, (d) => d[1])])
+                     .domain([0, d3.max(dataset, (d) => d)])
                      .range([height - padding*2 , 0]);
 
     const svg = d3.select("body")
@@ -41,19 +39,20 @@ fetch('https://api.covid19api.com/summary')
        .enter()
        .append("rect")
        .attr("x", (d,i) =>  i*10 + padding )
-       .attr("y",(d) => height - linearScale(d[1]  )-padding)
+       .attr("y",(d) => height - linearScale(d  )-padding)
        .attr('width', '8')
-       .attr('height', (d,i)=> linearScale(d[1]))
+       .attr('height', (d,i)=> linearScale(d))
        .attr('fill', '#00D6D6')
        .attr("rx",'2');
 
-    svg.selectAll("text")
-       .data(dataset)
-       .enter()
-       .append("text")
-       .text((d) =>  (d[0]))
-       .attr("x", (d,i) => i * 10 + padding)
-       .attr("y", (d) => height - linearScale(d[1]) - padding)
+
+//    svg.selectAll("text")
+//       .data(dataset)
+//       .enter()
+//       .append("text")
+//       .text((d) =>  (d))
+//       .attr("x", (d,i) => i * 10 + padding)
+//       .attr("y", (d) => height - linearScale(d) - padding)
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
