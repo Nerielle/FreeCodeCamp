e70e8e2b18301abe362afeb610afe9b4dc2c53d0
Nerielle <nelya.iva@gmail.com>
2020-05-07 15:23:54 +0300

    tooltip

--- 17 - D3 Bar chart/index.js
+++ 17 - D3 Bar chart/index.js
@@ -7,7 +7,7 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 
   
   let dataset = data.data;
-  console.log(dataset);
+  //console.log(dataset);
     
 /*     var years = dataset.reduce(((years, subArray)=>{
       let year = new Date(subArray[0]).getFullYear();
@@ -22,6 +22,21 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
       
   }),[]);
   console.log(years);*/
+  
+    function mouseOverHandler(d){
+        conslole.log('mouseover');
+  console.log(this, d, i);
+
+  
+    let tooltip = d3.select('#tooltip');
+        tooltip.html(new Date(d[0]).toLocaleDateString() + '<br/> ' + d[1]);
+      tooltip.style('opacity', '1')
+        .style('left', (i*barWidth +leftPadding) + 'px');
+        
+     
+    
+}  
+  
     
   let leftPadding = 50;
   let topPadding = 20;
@@ -34,8 +49,8 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     
   let minDate = new Date(data.from_date);
   let maxDate = new Date(data.to_date);
-    console.log(minDate, maxDate)
-  let xScale = d3.scaleTime()
+
+    let xScale = d3.scaleTime()
                 .domain([minDate, maxDate])
                 .range([0, innerWidth]);
   let yScale = d3.scaleLinear()
@@ -44,13 +59,22 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     let xAxis = d3.axisBottom(xScale);
     let yAxis = d3.axisLeft(yScale);
     
- var svg = d3.select('section')
+ var section = d3.select('section');
+    var svg = section
             .append('svg')
             .attr('class', 'graph')
             .attr("width", width)
             .attr("height", height);
  var group =   svg.append('g')
-        .attr('transform', 'translate(' + leftPadding + ', '+topPadding+')');
+        .attr('id', 'group')
+        .attr('transform', 'translate(' + leftPadding + ', '+topPadding+')')
+        .style('pointer-events', 'all');
+    
+    var tooltip = d3.select('body').append('div')
+                .attr("id", "tooltip")
+                .style('opacity', '0')
+                .style('position', 'absolute');
+    
     group.append('g')
         .attr('id','x-axis')
         .attr('stroke-width', '1')
@@ -76,76 +100,15 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
         .attr('y', (d, i)=> yScale(d[1]))
         .attr('width', barWidth)
         .attr('height', d=> innerHeight - yScale(d[1]))
-        .attr("rx",'2');
-        
-    
-            
-    
-/*  var maxConfirmedValue = d3.max(dataset, d=> d);
-
-//    const xScale = d3.scaleLinear()
-//                     .domain([0, dataset.length*25])
-//                     .range([0, width - padding]);
-    var dates = data.data.map(date=> new Date(date[0]));
-    
-    const linearScale = d3.scaleLinear()
-     .domain([0,d3.max(dataset, (d) => d[1])])
-                     .range([0 , height - padding ]);
-
-    const yScale = d3.scaleLinear()
-                     .domain([0, d3.max(dataset, (d) => d[1])])
-                     .range([height - padding , 0]);
-    
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-    
-
-
-  var xMax = new Date(d3.max(dates));
-  xMax.setMonth(xMax.getMonth() + 3);
-  var xScale = d3.scaleTime()
-    .domain([d3.min(dates), xMax])
-    .range([0, width]);
-    svg.append("g")
-        .attr('id','x-axis')
-       .attr("transform",'translate('+padding+','+ (height - padding)  +")")
-       .call(xAxis);
-
-    svg.append('g')
-       .attr('id','y-axis')
-    .attr('transform', 'translate('+padding +' ,'+padding+')')
-    .call(yAxis);
-   
-    const svg = d3.select("body")
-                  .append("svg")
-                  .attr("width", width)
-                  .attr("height", height);
-
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", (d,i) =>  i*25 + padding )
-       .attr("y",(d) => height - linearScale(d[1]  )-padding)
-       .attr('width', '20')
-       .attr('height', (d,i)=> linearScale(d[1]))
-       .attr('fill', '#00D6D6')
-       .attr("rx",'2')
-    .attr('data-date', d=>d[0])
-    .attr('data-gdp', d=>d[1])
-        .attr('class', 'bar');*/
-
-
-//    svg.selectAll("text")
-//       .data(dataset)
-//       .enter()
-//       .append("text")
-//       .text((d) =>  (d))
-//       .attr("x", (d,i) => i * 25 + padding)
-//       .attr("y", (d) => height - linearScale(d) - padding)
-
-
-
-
-  });
- 
+        .attr("rx",'2')
+        .on('mouseover', function mouseOverHandler(d,i){     
+            let tooltip = d3.select('#tooltip');
+            tooltip.html(new Date(d[0]).toLocaleDateString() + '<br/> ' + d[1]);
+            tooltip.style('opacity', '1')
+                .style('left', (i * barWidth + leftPadding) + 'px')
+               .style('top', d3.event.pageY + 'px');})
+        .on('mouseout',   function mouseOutEventHandler(d){
+            d3.select('#tooltip').transition()
+                .duration(200)
+                .style('opacity', 0);});
+})
