4ecbb6769985f792b96225af632c6f6ff36bb969
Nerielle <nelya.iva@gmail.com>
2020-05-14 19:59:51 +0300

    + header, tooltip

--- 19 - D3 Heat Map/index.js
+++ 19 - D3 Heat Map/index.js
@@ -26,14 +26,15 @@ function mouseOutEventHandler(d){
     tooltip.style("top", d3.event.pageY+"px").style("left", d3.event.pageX + 'px');}
     
   let leftPadding = 70;
-  let topPadding = 75;
+  let topPadding = 80;
   let padding = 30;
   let height = 600;
   let width = 1000;
   let innerHeight = height - padding - topPadding;
   let innerWidth = width - padding - leftPadding;
+    
   var colorScale = d3.scaleLinear().domain([1,10])
-                    .range(['#4C34C5', '#FA2837']);
+                    .range(['#4C34C5', '#FFCA28']);
     
     var section = d3.select('section');
     var svg = section
@@ -42,12 +43,12 @@ function mouseOutEventHandler(d){
             .attr("width", width)
             .attr("height", height);
     var header = svg.append('g') ;
-    
-                    header.append('text')
-                    .attr('x', innerWidth/2)
-                    .attr('y', '30')
-                    .text('Monthly Global Land-Surface Temperature')
-                    .attr('id', 'title');
+
+    header.append('text')
+        .attr('x', innerWidth/2)
+        .attr('y', '30')
+        .text('Monthly Global Land-Surface Temperature')
+        .attr('id', 'title');
     header.append('text')
         .attr('id','description')
         .attr('x', innerWidth/2)
@@ -67,7 +68,13 @@ function mouseOutEventHandler(d){
             acc.push(x.year);
        return acc;
     },[]);
+    
  
+var tooltip = d3.select('body').append('div')
+            .attr("id", "tooltip")
+            .style('opacity', '0')
+            .style('position', 'absolute');
+
 var xScale = d3.scaleLinear().domain(d3.extent(years)).range([0,innerWidth]);
 
    var xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d')); 
@@ -88,56 +95,32 @@ var xScale = d3.scaleLinear().domain(d3.extent(years)).range([0,innerWidth]);
         .attr('transform', 'translate('+0+','+0+')')
         .call(yAxis);
     
-
-   /* var minMaxYears = d3.extent(data.map(d=>d.Year));
-      
-    let xScale = d3.scaleLinear()
-                .domain([minMaxYears[0] - 1, minMaxYears[1] + 1])
-                .range([0, innerWidth]);
+var dataset = data.monthlyVariance.map(x=> {
+    var obj={};
+    obj.year= x.year;
+    obj.month= x.month;
+    obj.temp= data.baseTemperature + x.variance;
+    return obj;
+});
     
+group.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('class', 'cell')
+    .attr('data-month', d=> d.month)
+    .attr('data-year', d=> d.year)
+    .attr('data-temp', d=> d.temp)
+    .attr('x',d => xScale(d.year))
+    .attr('y', d => yScale(d.month - 1))
+    .attr('width',d=> xScale(d.year) - xScale(d.year - 1))
+    .attr('height', innerHeight/12)
+    .attr('fill', d=> colorScale(d.temp))
+    .on('mouseover', mouseOverHandler)
+    .on('mouseout',  mouseOutEventHandler )
+    .on('mousemove', mouseMovingHandler);
     
-    var timeFormat = '%M:%S';
-    let xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
-    
-    var times  = data.map( t=> {
-        
-        var ms = t.Seconds * 1000;
-        var date = new Date(ms);
-     
-        return [date, t.Seconds];
-    })
-    
-    var datesOnly = times.map(x=> x[0]);
-    
-    console.log(d3.extent(datesOnly))
-    let yScale = d3.scaleTime()
-                .domain( d3.extent(datesOnly))
-                .range([innerHeight, 0]);
-    
-    var filteredTimeTicks = times.filter(t => t[0].getSeconds()%5 == 0).map(x=>x[0]);
-    
-    let yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat(timeFormat));
-    
-    .tickValues(  filteredTimeTicks).tickFormat(t=>d3.timeFormat(timeFormat)(t));
-    
- 
-
-    
-    var tooltip = d3.select('body').append('div')
-                .attr("id", "tooltip")
-                .style('opacity', '0')
-                .style('position', 'absolute');
-    
-    group.append('g')
-        .attr('id','x-axis')
-        .attr('stroke-width', '1')
-        .attr('transform', 'translate('+0+','+(innerHeight )+')')
-        .call(xAxis);
-    group.append('g')
-        .attr('stroke-width', '1')
-        .attr('id','y-axis')
-        .attr('transform', 'translate('+0+','+0+')')
-        .call(yAxis);
+   /* 
     
 
     group.selectAll('circle')
